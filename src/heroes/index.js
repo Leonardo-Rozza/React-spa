c0a7b282e11d778c60baca30ec6d1bbb9b2dc876
Leonardo-Rozza <Leonardo155964@gmail.com>
2023-08-17 11:34:28 -0300

    First commit

---
+++ src/heroes/index.js
@@ -0,0 +1,5 @@
+export * from "./pages/DcPage";
+export * from "./pages/MarvelPage";
+export * from "./routes/HeroesRoutes";
+export * from "./pages/HeroPage";
+export * from "./pages/SearchPage";
---
+++ src/heroes/pages/DcPage.jsx
@@ -0,0 +1,12 @@
+export const DcPage = () => {
+  return (
+    <>
+      <h1>Dc Comics</h1>
+      <hr />
+      <ul>
+        <li>Superman</li>
+        <li>Batman</li>
+      </ul>
+    </>
+  );
+};
